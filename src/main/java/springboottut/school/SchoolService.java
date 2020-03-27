fb79899d71c1a5be34aebebcf22539e83c21a879
the-bort-the <jandora.mike@gmail.com>
2020-03-27 15:23:32 -0500

    pushing to have troubleshooting done.

---
+++ src/main/java/springboottut/school/SchoolService.java
@@ -0,0 +1,28 @@
+package springboottut.school;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class SchoolService {
+    @Autowired
+    private SchoolRepository schoolRepository;
+
+    public SchoolService(SchoolRepository schoolRepository) {
+        this.schoolRepository = schoolRepository;
+    }
+
+    public List<School> getAllSchools() {
+        List<School> schools = new ArrayList<>();
+        schoolRepository.findAll()
+                .forEach(schools::add);
+        return schools;
+    }
+
+    public void addSchool(School school) {
+        schoolRepository.save(school);
+    }
+}
